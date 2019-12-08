d35724204ab70cb0bd1eab55524b44b861bc26dc
Veerendranath Kommi <veekomm1@in.ibm.com>
2019-12-08 10:56:15 +0530

    added Helloworld program

---
+++ Helloworld.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Helloworld {
+
+public static void main(String ar[]){
+		
+		for(int i=0; i<=10;i++){
+			System.out.println("HelloWorld   "+i);
+		}
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
